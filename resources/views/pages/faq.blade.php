afab8ceecf54bac244c10cdf715fbe22bddc0b9b
deep kanani <deepkanani@jspinfotech.com>
2025-07-22 16:26:27 +0530

    first changes

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -17,6 +17,7 @@ $content = 'Find answers to common questions about guest posting, link building,
     .pt-60 {
         padding-top: 60px !important;
     }
+
 </style>
 
 <body>
@@ -35,13 +36,10 @@ $content = 'Find answers to common questions about guest posting, link building,
                         <div class="custom-accordian accordion accordion-flush mt-0" id="accordionFlushExample">
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingOne">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseOne" aria-expanded="false"
-                                        aria-controls="flush-collapseOne">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                                         Detailed Information About the Payment Methods That Are Available? </button>
                                 </h2>
-                                <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             We accept and offer payment via PayPal. When you are Indian, various payment methods are also available. It includes Razorpay, credit/debit cards, wallets, UPIs, and net banking.
@@ -51,14 +49,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseTwo" aria-expanded="false"
-                                        aria-controls="flush-collapseTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                                         What Can You Do If You Are Not Satisfied With the Order Performed by the Publisher?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseTwo" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             There is no chance of this kind of thing happening. Even though you can recommend changes in the note section before approving the link.
@@ -68,14 +63,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseThree" aria-expanded="false"
-                                        aria-controls="flush-collapseThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                                         What About the Lite Package?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseThree" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             It is best for you; with it, you will get 3 guest posting of 500 to 700 words. And the delivery time is between a week or two.
@@ -85,14 +77,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingFour">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseFour" aria-expanded="false"
-                                        aria-controls="flush-collapseFour">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
                                         What About the Standard Package?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseFour" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             The standard package is the most loved, which includes 5 handwritten content of 700 to 1000 words. You can even ask to add 2 keywords/URLs.
@@ -102,14 +91,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingFive">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseFive" aria-expanded="false"
-                                        aria-controls="flush-collapseFive">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
                                         What About Premium?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseFive" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             The handwritten content will be between 1000 and 1500 words, and the delivery time is around 3 to 4 weeks.<br />
@@ -119,14 +105,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingSix">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseSix" aria-expanded="false"
-                                        aria-controls="flush-collapseSix">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
                                         Explain About Customized Package?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseSix" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             The handwritten content with 1000+ words and 5+ posts and the pricing will be decided after you mention your need. <br />
@@ -136,14 +119,11 @@ $content = 'Find answers to common questions about guest posting, link building,
                             </div>
                             <div class="accordion-item">
                                 <h2 class="accordion-header" id="flush-headingSeven">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#flush-collapseSeven" aria-expanded="false"
-                                        aria-controls="flush-collapseSeven">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
                                         Is It Possible To See The Guest Post Sample?
                                     </button>
                                 </h2>
-                                <div id="flush-collapseSeven" class="accordion-collapse collapse"
-                                    aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
+                                <div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <p>
                                             Why not? Simply click on the view sample on the right side of the publisher website in the marketplace, where you can see the link to its sample post. <br />
